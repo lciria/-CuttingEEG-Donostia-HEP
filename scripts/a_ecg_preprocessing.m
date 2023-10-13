d541bc2681ef14df1abfa0dcc9a114b4113cdd49
lciria <40294758+lciria@users.noreply.github.com>
2023-10-13 10:27:15 +0200

    Add files via upload

--- scripts/a_ecg_preprocessing.m
+++ scripts/a_ecg_preprocessing.m
@@ -1,24 +1,24 @@
-% -----------------
+% ------------------------------------------------------------------------------------------
 % Tutorial HEP Donostia 2023
 % Luis Ciria
 % lciria@ugr.es
-% ------------------------------
+% ------------------------------------------------------------ra------------------------------
 % Extracts the ECG signal from the EEG raw data file and detects heartbeats
-% ------------------------------
+% ------------------------------------------------------------------------------------------
 clear
 close all
 clc
 
 addpath('/Users/luisciria/Desktop/CuttingEEG-Donostia HEP Tutorial/heplab/toolboxes/fieldtrip'); %fieldtrip
-addpath('/Users/luisciria/Desktop/CuttingEEG-Donostia HEP Tutorial/heplab/toolboxes/ecglab/functions'); %ecglab
+addpath(genpath('/Users/luisciria/Desktop/CuttingEEG-Donostia HEP Tutorial/heplab/toolboxes/ecglab')); %ecglab
 addpath(genpath('/Users/luisciria/Desktop/CuttingEEG-Donostia HEP Tutorial/heplab/toolboxes/ecgDeli')); %ecgDeli
 
-ft_defaults
+ft_defaults; % sets some general fieldtrip settings 
 
 datapath = '/Users/luisciria/Desktop/CuttingEEG-Donostia HEP Tutorial/heplab/data';             % Data location
 cd(datapath)
 
-%% load the EEG file 'ENT_1_raw.set'
+%% load the EEG file
 load('ENT_1_raw.mat');
 
 % to visualise the ECG signal you can use:
@@ -34,20 +34,24 @@ ecgout = data_eeg.trial{1}(chan, :);
 % filter the ECG signal using the ecg_filt function (Butterworth filter: 0.5 - 50 Hz)
 ecg = ecg_filt(double(ecgout),data_eeg.fsample);
 
-% prepare ECG .mat file for R or T peaks detection in ecglab
-% the variables have to be called 'ecg' and 'fs'
-% ecg must be an Nx1 vector
-ecg = ecg';
-fs  = data_eeg.fsample; % sampling frequency
+% ---------------------------------------------------------------------
+%% prepare ECG .mat file for R or T peaks detection
+% Two different toolboxes for detecting R (or T) ECG peaks: 1) ecglabRR 2) EEG-Deli
+% ---------------------------------------------------------------------
 
+%  ---------------------------- ecglabRR  ----------------------------
+% Save ecg and fs variables (semi-automatic, slow but accurate) 
+ecg = ecg'; % ecg must be an Nx1 vector
+fs  = data_eeg.fsample; % sampling frequency
 save ('ENT_1_ecg.mat','ecg','fs');
 
-%% Next step ---> load the ECG .mat file called ENT_1_sampleECG.mat to ecglabRR
+% Next step ---> load the ecg.mat file called ENT_1_ecg.mat to ecglabRR
 % use the ecglabRR GUI to generate the file with the heartbeat latencies for each participant
 
-% ------------------------------ OR ------------------------------
-% use the ECG-Deli
-% ------------------------------
+
+%% ------------------------------ ECG-Deli ------------------------------
+% Do NOT need to save variables in advance (automatic and fast but check for accuracy). It also detect other ECG waves (e.g., T peak) 
+
 [FPT_MultiChannel,FPT_Cell]=Annotate_ECG_Multi(ecg,fs); % R and T peaks detection (column 6 = R peaks; column 11 = T peaks)
 
 rr = FPT_MultiChannel(:,6);   % r peak latencies in samples
